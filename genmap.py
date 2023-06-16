a67ac1bf5d87c49e2fcacfad10df9a0feebf859a
Hazmi Alfarizqi <contact@hzmi.xyz>
2023-06-16 16:22:48 +0700

    feat: Rewrite genmap.php to genmap.py, add WKD hash entry
    This commit rewrites the genmap.php to genmap.py and adds a "W" entry meaning a WKD hash entry.
    This commit also adds requirements.txt for pip install -r requirements.txt
    
    The WKD hash is an SHA-1 hash with a Z-Base-32 encoded digest of the local part of an email address.
    This will be used to serve https://gnuweeb.org/.well-known/openpgpkey/<hash> by the server later.
    
    The server will first read the map.txt, filter only the gnuweeb.org email address, and then serve the public keys.
    For automatic map.txt updates, we can use something like GitHub Actions to run the genmap.py for every change in the keys directory and then push the new map.txt to the repo.
    The server will also have a route for webhook where we can utilize GitHub webhook to trigger the server to add or update served public keys.
    
    I will need your opinions on this.
    
    REF: https://datatracker.ietf.org/doc/draft-koch-openpgp-webkey-service
    
    Signed-off-by: Hazmi Alfarizqi <contact@hzmi.xyz>

---
+++ genmap.py
@@ -0,0 +1,61 @@
+# SPDX-License-Identifier: GPL-2.0
+import pgpy
+import os
+import hashlib
+import zbase32
+import datetime
+
+keys = []
+uids = []
+map = {}
+uid_fingerprints = {}
+
+def wkd_hash(email):
+    local, _ = email.split("@")
+    sha1 = hashlib.sha1(local.encode()).digest()
+    return zbase32.encode(sha1)
+
+for filename in os.listdir("keys"):
+    if filename.endswith(".asc"):
+        key, _ = pgpy.PGPKey.from_file("keys/" + filename)
+        keys.append(key)
+
+for key in keys:
+    for uid in key.userids:
+        email = uid.email
+        if email:
+            uids.append({"uid": uid.userid, "wkd_hash": wkd_hash(email)})
+            uid_fingerprints[uid.userid] = uid_fingerprints.get(uid.userid, []) + [
+                str(key.fingerprint)
+            ]
+
+for uid in uids:
+    map[uid["uid"]] = {
+        "wkd_hash": uid["wkd_hash"],
+        "fingerprints": uid_fingerprints[uid["uid"]],
+    }
+
+map = dict(sorted(map.items()))
+
+with open("map.txt", "w") as f:
+    f.write(
+        """# SPDX-License-Identifier: GPL-2.0
+#
+# GNU/Weeb developer PGP keys
+#
+# U = User
+# W = Web Key Directory email local part hash
+# P = Public key file
+#
+
+"""
+    )
+    for uid, values in map.items():
+        mapping = ""
+        mapping += f"U: {uid}\n"
+        mapping += f"W: {values['wkd_hash']}\n"
+        for fingerprint in values["fingerprints"]:
+            mapping += f"P: {fingerprint}.asc\n"
+        mapping += "\n"
+        f.write(mapping)
+    f.write(f"# Generated at: {datetime.datetime.utcnow().isoformat()}\n")
